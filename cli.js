45944969a346c195897dd5fb87c25617d616f69d
Hayley <haowens@email.unc.edu>
2023-03-07 10:06:13 -0500

    trial

---
+++ cli.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+import moment from "moment-timezone";
+
+// const [,, ...arguments] = process.argv;
+
+//node cli.js -n 35.92 -w 79.05 -z America/New_York
+
+var ns = process.argv[2];
+var lattitude = process.argv[3];
+var ew = process.argv[4];
+var longitude = process.argv[5];
+var z = process.argv[6];
+var where = process.argv[7];
+
+main();
+
+function main () {
+
+if (ns == "-h") {
+    console.log("Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE");
+    console.log("   -h            Show this help message and exit.");
+    console.log("   -n, -s        Latitude: N positive; S negative.");
+    console.log("   -e, -w        Longitude: E positive; W negative.");
+    console.log("   -z            Time zone: uses tz.guess() from moment-timezone by default.");
+    console.log("   -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.");
+    console.log("   -j            Echo pretty JSON from open-meteo API and exit.");
+    return;
+    ;
+}
+
+//extract system timezone
+const timezone = moment.tz.guess();
+
+fetch("http://example.com/movies.json")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+}
+
+
+
+
